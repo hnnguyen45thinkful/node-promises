e72fdc09e157a4d7c64efddcb0e4f511f2694f0d
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-29 20:45:49 -0800

    test out all examples from curriculum.

---
+++ example1.js
@@ -0,0 +1,19 @@
+function wait(seconds, callback) {
+  setTimeout(function(){
+    callback(new Date());
+  }, seconds * 1000);
+}
+
+console.log('Console log before calling wait:', new Date());
+
+wait(3, function(date) {
+  console.log('Console log inside first callback:', date);
+  wait(2, function(date) {
+    console.log('Console log inside second callback:', date);
+    wait(5, function(date) {
+      console.log('Console log inside third callback:', date);
+    });
+  });
+});
+
+console.log('Console log after calling wait:', new Date());
---
+++ example2.js
@@ -0,0 +1,8 @@
+var promise = new Promise(function(resolve) {
+  // some async operation here
+  resolve("Success!");
+});
+
+promise.then(function(result) {
+  console.log(result);
+});
